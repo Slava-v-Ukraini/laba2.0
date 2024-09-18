6ea44b6c23957b86a5740aced0fbfc02dbc6a15a
myroslav.holub.ri.2024@lpnu.ua <myroslav.holub.ri.2024@lpnu.ua>
2024-09-18 18:10:16 +0300

    second commit

--- PR-2.1.cpp
+++ PR-2.1.cpp
@@ -10,17 +10,17 @@ int main()
     SetConsoleCP(1251);
     double a; // Змінна для кута альфа
     double z1; // Змінна для значення z1
-    double z2; // Змінна для значення z2
+    //double z2; зміна проекту для створення нового commit
 
     cout << " Введіть значення кута a= "; cin >> a; 
 
     z1 = 1 - (1.0 / 4.0) * pow(sin (2 * a) , 2) + cos(2 * a); // Приклад номер 1
-    z2 = pow(cos(a), 2) + pow(cos(a), 4); // Приклад номер 2
+    //z2 = pow(cos(a), 2) + pow(cos(a), 4); зміна проекту для створення нового commit
 
     cout << endl;
     
     cout << "z1 = " << z1 << endl;
-    cout << "z2 = " << z2 << endl;
+    //cout << "z2 = " << z2 << endl; //зміна проекту для створення нового commit
 
     cin.get();
     cin.get();
